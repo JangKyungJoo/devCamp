ce1c798b9e440262e3232d01fe7a72307427de06
jiyoung111 <jiyoung928@gmail.com>
2017-03-18 21:22:02 +0900

    guide update

--- app/src/main/java/com/example/devcamp/guide/GuideDetailActivity.java
+++ app/src/main/java/com/example/devcamp/guide/GuideDetailActivity.java
@@ -10,6 +10,6 @@ public class GuideDetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_guide_detail);
+        setContentView(R.layout.layout_guide_detail);
     }
 }
